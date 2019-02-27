d847f64362b1d68bc3fc86b2828b344feb5a4eb8
brellin <cekakid@gmail.com>
2019-02-27 15:10:54 -0600

    finished MVP, now working on stretch

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,53 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Without providing context, `this` is automatically set to the window or console.
+* 2. Context to `this` is provided (implicitly) by a preceding `.`, followed by a predefined method.
+* 3. Context to `this` by means of a constructor function is provided by the `new` keyword.
+* 4. Context to `this` is provided (explicitly) by means of a built-in `.call` and/or `.apply` method.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+console.log(this);
 
 // Principle 2
 
-// code example for Implicit Binding
+const joe = {
+    name: 'Joe',
+    say: function() {console.log(`I'm ${this.name}.`)}
+}
+
+joe.say();
 
 // Principle 3
 
-// code example for New Binding
+function Person(att){
+    this.name = att.name;
+    this.age = att.age;
+}
+
+let jim = new Person({
+    name: 'Jim',
+    age: 20
+});
+
+console.log(jim.name, jim.age);
 
 // Principle 4
+function Pet(petAtt){
+    Person.call(this, petAtt);
+    this.toy = petAtt.toy;
+}
 
-// code example for Explicit Binding
+Pet.prototype.play = function(){console.log(`${this.name} plays with its\' ${this.toy}`);};
+
+let rocky = new Pet({
+    name: 'Rocky',
+    age: 70,
+    toy: 'bone'
+});
+
+rocky.play();
